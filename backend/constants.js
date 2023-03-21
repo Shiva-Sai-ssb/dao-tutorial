dcab862274f95581c2c9959d9f0db2987f698ce3
Shiva-Sai-ssb <shivas.bojjalwar@gmail.com>
2023-03-21 13:31:12 +0530

    complete

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xeeF93A395e11F778C8A6856BEda9D33F02669b7F";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
